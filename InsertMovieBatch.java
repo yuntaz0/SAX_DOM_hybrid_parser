9ce379233eb108bafad730021cc9d002fac48f5f
Yuntao Zhao <yuntaz@proton.me>
2024-11-09 19:51:56 -0800

    fix: switch to txt based

--- InsertMovieBatch.java
+++ InsertMovieBatch.java
@@ -3,19 +3,26 @@ import java.io.IOException;
 import java.util.List;
 
 public class InsertMovieBatch {
+    private static final String TEMP_FILE = "sql_commands.sql";
     private static final String INSERT_SQL_TEMPLATE = "INSERT INTO stage_movies (movie_id, title, year, director) VALUES ('%s', '%s', %d, '%s');";
-    private static final String TEMP_FILE = "Test.txt";
+
     public void batchInsertMovies(List<Movie> movies) {
+
         try (FileWriter writer = new FileWriter(TEMP_FILE, true)) {
+            writer.write("SET GLOBAL autocommit = 0; USE moviedb;" + System.lineSeparator());
+
             for (Movie movie : movies) {
                 String insertSql = String.format(INSERT_SQL_TEMPLATE,
                         movie.getId(),
-                        movie.getTitle(), // Escape single quotes
+                        movie.getTitle(),
                         movie.getYear(),
-                        movie.getDirector()); // Escape single quotes
+                        movie.getDirector());
 
                 writer.write(insertSql + System.lineSeparator());
             }
+
+            writer.write("SET GLOBAL autocommit = 1;" + System.lineSeparator());
+
         } catch (IOException e) {
             System.err.println("Error writing to file: " + TEMP_FILE);
             e.printStackTrace();
